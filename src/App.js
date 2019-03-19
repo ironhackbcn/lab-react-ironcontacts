b84d066b275ee411d178231e9fab7e359180d32d
Adriana <adrirx5@gmail.com>
2019-03-19 16:15:15 +0100

    fixed delete button

--- src/App.js
+++ src/App.js
@@ -2,62 +2,56 @@ import React, { Component } from 'react';
 import './App.css';
 import contacts from './data/contacts.json';
 import TableRow from './components/TableRow.js';
-// import { constants } from 'perf_hooks';
 
 class App extends Component {
   state = {
     data: contacts.slice(0,5)
   }
 
-  randomContactHandler = () => {
-    const randomContact = contacts[Math.floor(Math.random()*contacts.length)];
+  randomContactHandler =() => {
+    const randomContact = contacts[Math.floor(Math.random()*contacts.length)];    
     this.setState({
       data: [...this.state.data, randomContact]
     })
   }
 
   sortByNameHandler =() => {
-    const sorted = this.state.data.sort((a, b) => {
-      const textA = a.name;
-      const textB = b.name;
-      return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    const sorted = this.state.data.sort((a, b) => {      
+      return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
     });
       this.setState({
         data: sorted
       })
   }
 
-  sortByPopularityHandler =()=> {
+  sortByPopularityHandler =() => {
     const sorted = this.state.data.sort((a, b) => {
-      const textA = a.popularity;
-      const textB = b.popularity;
-      return (textA < textB) ? 1 : (textA > textB) ? -1 : 0;
+      return (a.popularity < b.popularity) ? 1 : (a.popularity > b.popularity) ? -1 : 0;
     });
       this.setState({
         data: sorted
     })
   }
 
-  deleteHandler = (name) => {
-    const filtered = this.state.data.filter((contact) => {
-      return name !== contact.name;
-    });
+  deleteHandler = (id) => {    
+    const contactsCopy = [...this.state.data]
+    contactsCopy.splice(id, 1)
     this.setState({
-      data: filtered
+      data: contactsCopy
     })
-  }
-  
+  }  
 
   render() {
-    const items = this.state.data.map((contact, index) => 
-      <TableRow key={index} contact={contact} delete={this.deleteHandler}/>
+
+    const contacts = this.state.data.map((contact, index) => 
+      <TableRow key={`id-${index}`} id={index} contact={contact} onDelete={this.deleteHandler}/>
     );
     return (
       <div className="App">
         <h1>IronContacts</h1>
-        <button onClick={() => this.randomContactHandler()}>Add random Contact</button>
-        <button onClick={() => this.sortByNameHandler()}>Sort by name</button>
-        <button onClick={() => this.sortByPopularityHandler()}>Sort by Popularity</button>
+        <button onClick={this.randomContactHandler}>Add random Contact</button>
+        <button onClick={this.sortByNameHandler}>Sort by name</button>
+        <button onClick={this.sortByPopularityHandler}>Sort by Popularity</button>
         <table>
           <thead>
             <tr>
@@ -67,7 +61,7 @@ class App extends Component {
             </tr>
           </thead>
           <tbody>
-            {items}
+            {contacts}
           </tbody>
         </table>
       </div>
--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
 
 class TableRow extends Component {
-
-  deleteContact =() => {
-    this.props.delete(this.props.contact.name)
-  }
-  render() {
+  render() {    
     return(
       <tr>
         <td><img className="image" src={this.props.contact.pictureUrl} alt={this.props.contact.name}/></td>
         <td>{this.props.contact.name}</td>
         <td>{this.props.contact.popularity}</td>
-        <td><button onClick={this.deleteContact}>Delete</button></td>
+        <td><button onClick={() => this.props.onDelete(this.props.id)}>Delete</button></td>
       </tr>
     );
   }
 }
-
 
 export default TableRow;
