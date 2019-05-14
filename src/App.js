e8e96779a80031d1ce25ee806f1aa5916956ecb4
Jesus Cebader <zebader@hotmail.com>
2019-05-15 00:46:05 +0200

    trying it 2

--- src/App.js
+++ src/App.js
@@ -2,22 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import contacts from './data/contacts.json';
 
+const ContactList = contacts.slice(0,5);
+const RandomContact = contacts[Math.floor(Math.random()*contacts.length)];
+ContactList.push(RandomContact)
+
+const ContactListFinal = ContactList.map((contact, index) =>
+<tr key={index}>
+  <td ><img src={contact.pictureUrl}/></td>
+  <td >{contact.name}</td>
+  <td >{contact.popularity}</td>
+</tr>
+);
+
+
 class App extends Component {
 
+  state={
+    contacts : ContactListFinal
+  }
+
+  DeleteAll = () => {
+    this.setState({ contacts: [] });
+  }
+  RandomActor = () => {
+    /* this.setState({ contacts: ContactListFinal }); */
+  }
+
   render() {
-
-    const ContactList = contacts.slice(0,5).map((contact, index) =>
-      <tr>
-        <td key={index}><img src={contact.pictureUrl}/></td>
-        <td key={index}>{contact.name}</td>
-        <td key={index}>{contact.popularity}</td>
-      </tr>
-        
-      );
-
+    
     return (
       <div className="App">
         <h1>IRONCONTACTS</h1>
+        <button onClick={this.RandomActor}>Add Random Contact</button>
         <table>
           <thead>
           </thead>
@@ -27,7 +43,7 @@ class App extends Component {
               <th>Name</th>
               <th>Popularity</th>
             </tr>
-              {ContactList}
+              {this.state.contacts}
           </tbody>
         </table>
       </div>
