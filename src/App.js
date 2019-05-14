8e9e015e627018b5b5e58f03a57484c5d525dd20
Ann-Sofi Lantz <Anso@Anso.local>
2019-05-14 21:45:12 +0200

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import ContactsTable from './components/ContactsTable';
 import contacts from './data/contacts.json'
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <h1>IronContacts</h1>
-        <div><button className='btn'>Add Random Contact</button></div>
+
         <ContactsTable contactsArray={contacts} />
       </div>
     );
--- src/components/ContactsTable.js
+++ src/components/ContactsTable.js
@@ -2,22 +2,47 @@ import React from 'react';
 import shortid from 'shortid';
 import TableRow from './TableRow';
 
-const ContactsTable = (props) => {
-  return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-        </tr>
 
-        {
-          props.contactsArray.slice(0, 5).map((oneContact) => <TableRow key={shortid.generate()} contact={oneContact} />)
-        }
-      </tbody>
-    </table>
-  )
+class ContactsTable extends React.Component {
+  //  we use `constructor` as we are setting initial `state` value from the `props`
+  constructor(props) {
+    super();
+    this.state = {
+      contacts: props.contactsArray.slice(0, 5)
+    }
+  }
+
+
+  addContact = () => {
+    const randomNumber = Math.floor((Math.random() * this.props.contactsArray.length - 1) + 5);
+    this.state.contacts.push(this.props.contactsArray[randomNumber])
+    this.setState({ contacts: this.state.contacts });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <button className='btn' onClick={this.addContact}>Add Random Contact</button>
+
+        <table>
+          <tbody>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+
+            {
+              this.state.contacts.map((oneContact) => <TableRow key={shortid.generate()} contact={oneContact} />)
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+
+  }
+
 };
 
 export default ContactsTable; 
