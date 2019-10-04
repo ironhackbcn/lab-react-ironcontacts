2bc192ca287a501a05f29f541b11a1cc5aea26be
johan Bautista <jsbparra@gmail.com>
2019-10-04 22:27:49 +0200

    done al 30%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import contacts from './data/contacts.json';
 import './App.css';
+// import { generateContact } from './helpers/index.js';
+
+let list = contacts.slice(0, 10).map((contact, index) => (
+  <tr key={index}>
+    <td>
+      <img src={contact.pictureUrl} alt="" className="img-list" />
+    </td>
+    <td className="text-list">{contact.name}</td>
+    <td className="text-list">{contact.popularity}</td>
+  </tr>
+));
 
 class App extends Component {
+  state = {
+    contacts: contacts,
+    title: 'Iron Contacts',
+  };
+
   render() {
+    const { title } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            edit  <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="bttn-section">
+          <button onClick={this.AddContact}>Add Contact</button>
+          <button>Sort by Name</button>
+          <button>Sort by Popularity</button>
+        </div>
+        <h1>{title}</h1>
+        {list}
       </div>
     );
   }
---
+++ src/helpers/index.js
@@ -0,0 +1,3 @@
+export const generateContact = () => {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
