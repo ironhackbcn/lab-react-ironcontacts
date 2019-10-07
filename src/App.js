73867a3573237315f6372e2b3407d2c1223b53c0
Elena Piaggio <elenapiaggio@gmail.com>
2019-10-07 22:35:29 +0200

    add iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ContactCard from './components/ContactCard';
+import contacts from './data/contacts.json'
+
+let firstFiveContacts = contacts.slice(0,5);
+console.log(firstFiveContacts[3]);
 
 class App extends Component {
+  state = {
+    initialContacts: firstFiveContacts,
+  } 
+
+  // handleAddContact = () =>{
+  //   const { firstFiveContacts } = this.state;
+  //   this.setState({
+  //     gradients: [...firstFiveContacts],
+      
+  //   }, () => {
+  //     console.log('🤣', this.state.gradients)
+  //   })
+  // }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>IronContacts</h1>
+        
+        <div className="wrapper">
+          {firstFiveContacts.map((card, index) =>{
+            return (
+              <ContactCard 
+                key={`${card.name}-${index}`} 
+                name={card.name} 
+                pictureUrl={card.pictureUrl}
+                popularity={card.popularity}
+              />
+            );
+          })}
+        </div>
+
+        {/* <ContactCard>
+          theContacts = {this.state.contacts}
+        </ContactCard> */}
       </div>
     );
   }
---
+++ src/components/ContactCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function ContactCard(props){
+    
+    const {name, pictureUrl, popularity} = props;
+    
+    return (
+        <div className="contact-card">
+            <table className="table-contact-card">
+                <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                </tr>
+                <tr>
+                    <td>
+                        <img src={pictureUrl} alt="actor-picture" width="30%"/>
+                    </td>
+                    <td>{name}</td>
+                    <td>{popularity}</td>
+                </tr>
+            </table>
+        </div>
+    );
+}
+
+export default ContactCard;
