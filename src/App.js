366e312af3f76ac71dfc6d6cd03a7aab27380d7b
Daniel Duque <duque@outlook.com>
2019-11-05 16:40:47 +0100

    Converted to a class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import ContactList from './components/ContactList';
-import ContactTable from './components/ContactTable';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h1>IronContacts</h1>
-        <ContactTable>
-          <ContactList />
-        </ContactTable>
+        <ContactList />
       </div>
     );
   }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, {
+  Component
+} from 'react';
 import contacts from '../data/contacts.json';
 import Contact from './Contact';
 
-const ContactList = () => {
-  const shortContacts = contacts.slice(0,5);
-  return (
-    <tbody>
-      {
-        shortContacts.map((contact, index) =>
-          <Contact
-            key={index}
-            pictureUrl={contact.pictureUrl}
-            name={contact.name}
-            popularity={contact.popularity}
-          />
-        )
-      }
-    </tbody>
-  )
-}
+export default class ContactList extends Component {
+  
+  // Get first 5 contacts
+  shortContacts = (contacts) => {
+    return contacts.slice(0,5);
+  }
 
-export default ContactList;
+  // Get a random contact
+  getRandom = () => {
+    let randomIndex = Math.floor((Math.random() * contacts.length) + 0);
+    return contacts[randomIndex];
+  }
+
+  render () {
+    return (
+      <div>
+        <button>Add Random Contact</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.shortContacts(contacts).map((contact, index) =>
+                <Contact
+                  key={index}
+                  pictureUrl={contact.pictureUrl}
+                  name={contact.name}
+                  popularity={contact.popularity}
+                />
+              )
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
