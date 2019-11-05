599a39ae1757d7ccf40a754e09cfdfc0c92f0533
juliacanas <julia_cl90@homtail.com>
2019-11-05 21:31:25 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contacts from './data/contacts.json'
 
 class App extends Component {
-  render() {
+  state = {
+  constactsList: contacts.slice(0, 5)
+  }
+
+
+
+  randomContact = () => {
+    const copy = [...this.state.constactsList];
+    let newContact = contacts[Math.floor(Math.random() * (contacts.length))];
+    copy.unshift(newContact);
+    this.setState({
+      constactsList: copy
+    })
+  };
+
+  sortByName = () => {
+    const sortCopy = [...this.state.constactsList];
+    const newArr = sortCopy.sort((a, b) => {
+      return a.name<b.name ? -1: a.name>b.name? 1:0;
+    })
+
+    this.setState({
+      constactsList: newArr
+    })
+  }
+
+  sortByPopularity = () => {
+    const sortPop = [...this.state.constactsList];
+    const orderPop = sortPop.sort((a, b) => {
+      return a.popularity < b.popularity ? -1 : a.popularity > b.popularity ? 1 : 0;
+    })
+
+    this.setState({
+      constactsList: orderPop
+    })
+  }
+
+  deleteContact = (index) => {
+    const copyContact = [...this.state.constactsList];
+    copyContact.splice(index, 1);
+    this.setState({
+      constactsList: copyContact
+    })
+  }
+
+    render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>IronContacts</h1>
+        <button className='btn' onClick={this.randomContact}>Add random Contact</button>
+        <button className='btn'onClick={this.sortByName}>Sort by Name</button>
+        <button className='btn'onClick={this.sortByPopularity}>Sort by Popularity</button>
+        <table className='table'>
+          <thead>
+          <tr className='headers'>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Action</th>
+          </tr>
+          </thead>
+          <tbody>
+          {
+            this.state.constactsList.map((oneContact, index) => {
+              return <tr className='row-table' key={index}> 
+              <td> <img width='80px' src={oneContact.pictureUrl} alt='' /> </td> 
+              <td> <p>{oneContact.name}</p> </td>
+              <td> <p>{oneContact.popularity}</p> </td>
+              <td> <button className='btn-delete' onClick={this.deleteContact}>Delete</button></td>
+              </tr>
+            })
+          }
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
